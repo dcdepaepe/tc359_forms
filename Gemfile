6cb7580ecfa46d956583a355ccf207abac125233
Devin Cory Depaepe <dcdepaepe@gmail.com>
2015-02-21 14:28:18 -0500

    project working output holidays to simple grid need design work

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 ruby '2.2.0'
 
 gem 'sinatra', '~> 1.4.5'
+gem 'holidapi'
+gem 'cat_api'
--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
+require 'holidapi'
+require 'cat_api'
 
 class MyWebApp < Sinatra::Base
   get '/' do
+  	@pictures = CatAPI.new.get_images(category: 'hats')
+  	@holidays = HolidApi.get(country:'US', year: Time.now.year, month: Time.now.month)
+  	@birthHolidays = HolidApi.get(country:'US', year: 1991, month: 11)
+
     erb :index
   end
 end
