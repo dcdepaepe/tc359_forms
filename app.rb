c9c41632f11d78c2748a5101d7594282df2cdee3
Devin Cory Depaepe <dcdepaepe@gmail.com>
2015-03-01 18:26:30 -0500

    internal server error on start-up

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require 'holidapi'
 class MyWebApp < Sinatra::Base
   get '/' do
 
+  	@holidays = HolidApi.get(country: params["country"], year: params["year"], month: params["month"])
+
     erb :index
   
   end
