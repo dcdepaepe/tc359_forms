e8c7a65bcac5fc064ecd2a7b11783ff6bfe13428
Devin Cory Depaepe <dcdepaepe@gmail.com>
2015-03-01 19:07:33 -0500

    possible fix

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'holidapi'
 class MyWebApp < Sinatra::Base
   get '/' do
 
-  	@holidays = HolidApi.get(country: params["country"], year: params["year"], month: params["month"])
+  	@holidays = HolidApi.get(params)
 
     erb :index
   
